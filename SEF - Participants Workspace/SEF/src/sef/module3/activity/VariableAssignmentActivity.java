e50f0dc3c614605877d72f79da3354f416042449
Veronika Novikova <1veronika.novikova@gmail.com>
2021-03-02 09:31:44 +0200

    My first comment

--- SEF - Participants Workspace/SEF/src/sef/module3/activity/VariableAssignmentActivity.java
+++ SEF - Participants Workspace/SEF/src/sef/module3/activity/VariableAssignmentActivity.java
@@ -13,11 +13,13 @@ public class VariableAssignmentActivity {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+		int x = 2;
+		 x = 3;
+		System.out.println(x);
+
 		// 1- Declare a variable of type int and assign it default value.
 		// 2- Update the value 
 		// 3- Print updated value to the console
 
 	}
-
 }
